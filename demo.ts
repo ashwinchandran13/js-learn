fd1dfb83ee058d529249bb4d7f3c9d9ab3da45ac
Ashwin Chandran <ashwin.chandran@neoito.com>
2022-10-30 20:29:00 +0530

    ES6 setup

---
+++ demo.ts
@@ -0,0 +1,9 @@
+var a = 1;
+var b = 20;
+if (a === 1) {
+    var a = 20
+    let b = 1
+    console.log({a,b})
+}
+
+console.log(a, b)
---
+++ scripts/demo.js
@@ -0,0 +1,10 @@
+"use strict";
+var a = 1;
+var b = 20;
+if (a === 1) {
+    var a = 20;
+    let b = 1;
+    console.log({ a, b });
+}
+console.log(a, b);
+//# sourceMappingURL=demo.js.map
